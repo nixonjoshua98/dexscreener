fa8656c29d17bf7431532cf65deb156f7657c7db
Joshua <nixonjoshua98@gmail.com>
2023-09-18 19:08:11 +0100

    Updated rate limiter

--- dexscreener/ratelimit.py
+++ dexscreener/ratelimit.py
@@ -1,6 +1,7 @@
 import time
 import threading
 import collections
+import asyncio
 
 
 class RateLimiter:
@@ -9,23 +10,48 @@ class RateLimiter:
 
         self.period = period
         self.max_calls = max_calls
-        self.lock = threading.Lock()
+
+        self.sync_lock = threading.Lock()
+        self.async_lock = asyncio.Lock()
+
+    async def __aenter__(self):
+        async with self.async_lock:
+            sleep_time = self.get_sleep_time()
+
+            if sleep_time > 0:
+                await asyncio.sleep(sleep_time)
+
+            return self
 
     def __enter__(self):
-        with self.lock:
-            if len(self.calls) >= self.max_calls:
-                until = time.time() + self.period - self._timespan
-                sleeptime = until - time.time()
-                if sleeptime > 0:
-                    time.sleep(sleeptime)
+        with self.sync_lock:
+            sleep_time = self.get_sleep_time()
+
+            if sleep_time > 0:
+                time.sleep(sleep_time)
+
             return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        with self.lock:
-            self.calls.append(time.time())
+        with self.async_lock:
+            self._clear_calls()
 
-            while self._timespan >= self.period:
-                self.calls.popleft()
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        async with self.sync_lock:
+            self._clear_calls()
+
+    def get_sleep_time(self) -> float:
+        if len(self.calls) >= self.max_calls:
+            until = time.time() + self.period - self._timespan
+            return until - time.time()
+
+        return 0
+
+    def _clear_calls(self):
+        self.calls.append(time.time())
+
+        while self._timespan >= self.period:
+            self.calls.popleft()
 
     @property
     def _timespan(self):
